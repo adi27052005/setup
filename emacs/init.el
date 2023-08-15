a0b4c885a72e8435c2fa318ed0e832ebfe799723
adi27052005 <132362628+adi27052005@users.noreply.github.com>
2023-08-15 18:57:16 +0530

    Add files via upload

--- emacs/init.el
+++ emacs/init.el
@@ -7,7 +7,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(deeper-blue)))
+ '(custom-safe-themes
+   '("41020bc5a5547dbc5eaf2554d188f516cc5d3b80fd5d5ad804444135a6abf5f4" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
